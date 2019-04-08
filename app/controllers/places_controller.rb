843d562d9301c500dd32101a15faaa44cdbc78eb
lambrou <alexanderlambrou0602@gmail.com>
2019-04-08 00:50:15 +0000

    Cleaned up Edit page

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -18,7 +18,12 @@ class PlacesController < ApplicationController
   def edit
     @place = Place.find(params[:id])
   end
-  
+
+  def updated
+    @place = Place.find(params[:id])
+    @place.update_attributes(place_params)
+    redirect_to root_path
+  end  
   private
 
   def place_params
