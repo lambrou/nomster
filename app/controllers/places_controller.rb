904c49cef06f056e841126452211519ea96b19fe
lambrou <alexanderlambrou0602@gmail.com>
2019-04-08 01:22:25 +0000

    Added restrictions to specific pages and actions

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
   def index
    @places = Place.all.page(params[:page]).per_page(3)
   end
@@ -17,10 +17,16 @@ class PlacesController < ApplicationController
   
   def edit
     @place = Place.find(params[:id])
+    if @place.user != current_user
+      return render plain: 'Not Allowed', status: :forbidden
+    end
   end
 
   def updated
     @place = Place.find(params[:id])
+    if @place.user != current_user
+      return render plain: 'Not Allowed', status: :forbidden
+    end
     @place.update_attributes(place_params)
     redirect_to root_path
   end  
