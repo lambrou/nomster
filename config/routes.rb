93d89b0112402722eefea345f443b8f2442e9e73
lambrou <alexanderlambrou0602@gmail.com>
2019-04-25 15:29:43 +0000

    Added photos to places

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'places#index' # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :places do
+    resources :photos, only: :create
     resources :comments, only: :create
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_16_024729) do
+ActiveRecord::Schema.define(version: 2019_04_25_151839) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,8 @@ ActiveRecord::Schema.define(version: 2019_04_16_024729) do
     t.integer "photo_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "picture"
+    t.integer "user_id"
     t.index ["place_id"], name: "index_photos_on_place_id"
   end
 
@@ -44,6 +46,7 @@ ActiveRecord::Schema.define(version: 2019_04_16_024729) do
     t.integer "user_id"
     t.float "latitude"
     t.float "longitude"
+    t.string "photo"
     t.index ["user_id"], name: "index_places_on_user_id"
   end
 
