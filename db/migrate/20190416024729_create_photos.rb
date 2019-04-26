f34ac639554a38c73591b037fbfa11e8af226d88
lambrou <alexanderlambrou0602@gmail.com>
2019-04-26 20:57:55 +0000

    Fixed database errors

--- db/migrate/20190416024729_create_photos.rb
+++ db/migrate/20190416024729_create_photos.rb
@@ -3,7 +3,6 @@ class CreatePhotos < ActiveRecord::Migration[5.2]
     create_table :photos do |t|
       t.text :caption
       t.integer :place_id
-      t.integer :photo_id
       t.timestamps
     end
     add_index :photos, :place_id
