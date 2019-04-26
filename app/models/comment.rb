8b4f188cf133c672d1fc598c568e4f4aca4374d7
lambrou <alexanderlambrou0602@gmail.com>
2019-04-26 16:23:27 +0000

    Added email notifications

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :place
+  after_create :send_comment_email
+
   RATINGS = {
     'one star': '1_star',
     'two stars': '2_stars',
@@ -10,5 +12,8 @@ class Comment < ApplicationRecord
   }
   def humanized_rating
     RATINGS.invert[self.rating]
+  end
+  def send_comment_email
+      NotificationMailer.comment_added(self).deliver_now
   end
 end
